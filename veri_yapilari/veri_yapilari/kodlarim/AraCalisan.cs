67a3900ce304139818c681fc4b2389aff8059020
furkantingir <furkantingir77@gmail.com>
2025-05-09 11:57:34 +0300

    son güncellemeler

---
+++ veri_yapilari/veri_yapilari/kodlarim/AraCalisan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace veri_yapilari.kodlarim
+{
+    public class AraCalisan
+    {
+        public string Bul(string ad,string soyad)
+        {
+            string info = null; //simdilik nulladim
+            return info;
+        }
+
+    }
+}
